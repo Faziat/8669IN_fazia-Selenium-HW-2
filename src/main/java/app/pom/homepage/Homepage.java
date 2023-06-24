d3be05dd9ae33ae45759bc97a6e78c800ceded40
Faziat <toubouachefazia@gmail.com>
2023-06-24 04:49:19 -0400

    Initial Commit

--- src/main/java/app/pom/homepage/Homepage.java
+++ src/main/java/app/pom/homepage/Homepage.java
@@ -1,9 +1,11 @@
 package app.pom.homepage;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class Homepage extends BasePage {
 
@@ -13,5 +15,33 @@ public class Homepage extends BasePage {
     public Homepage(){
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy (xpath = "//input[@id='search']")
+    public WebElement searchBar;
+  //  WebElement searchBar = driver.findElement(By.xpath("//input[@id='search']"));
+
+    @FindBy (xpath = "//button[@title='Search']")
+    public WebElement searchBtn;
+
+    @FindBy (xpath = "//a[contains(text(), 'Josie Yoga Jacket' )]")
+    public WebElement searchItem;
+
+    @FindBy (xpath ="//button[@title='Add to Cart']")
+    public WebElement addToCartBtn;
+
+    @FindBy (xpath ="//a[@class='action showcart']")
+   public WebElement addToCartIcon;
+
+   @FindBy (xpath ="//img[@alt='Dash Digital Watch']")
+    public WebElement itemIcon;
+    @FindBy (xpath ="//button[@title='Subscribe']")
+    public WebElement subscriptionBtn;
+
+  @FindBy (xpath ="//div[contains(text(), 'This email address is already subscribed.')]")
+    public WebElement errorMessage;
+
+    @FindBy (xpath ="//input[@name=\"email\"]")
+    public WebElement emailToSubscribe;
+
 
 }
