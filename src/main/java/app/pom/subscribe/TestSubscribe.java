80c00480b3495d4fd6a86d5fa6a13fee4af8c8c3
Faziat <toubouachefazia@gmail.com>
2023-06-24 10:40:57 -0400

    Test Subscribe updated

--- src/main/java/app/pom/subscribe/TestSubscribe.java
+++ src/main/java/app/pom/subscribe/TestSubscribe.java
@@ -2,14 +2,10 @@ package app.pom.subscribe;
 import base.BasePage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 public class TestSubscribe extends BasePage {
    @FindBy(xpath ="//button[@title='Subscribe']")
-    public WebElement subscriptionBtn;
-
-    @FindBy (xpath ="//div[contains(text(), 'This email address is already subscribed.')]")
+    public WebElement subscriptionBtn;@FindBy (xpath ="//div[contains(text(), 'This email address is already subscribed.')]")
     public WebElement errorMessage;
-
     @FindBy (xpath ="//input[@name=\"email\"]")
     public WebElement emailToSubscribe;
 
