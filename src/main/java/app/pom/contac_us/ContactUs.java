5514da37c8e9d9bf70b34a3d341921e370dd5cc4
Faziat <toubouachefazia@gmail.com>
2023-06-24 05:01:11 -0400

    Contact Us functionality tested

---
+++ src/main/java/app/pom/contac_us/ContactUs.java
@@ -0,0 +1,35 @@
+package app.pom.contac_us;
+import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class ContactUs extends BasePage{
+    public ContactUs(){
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//a[normalize-space()='Contact Us']")
+    public WebElement contactUsButton;
+    @FindBy(xpath = "//input[@id='name']")
+    public WebElement inputName;
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement inputEmail;
+    @FindBy(xpath = "//input[@id='telephone']")
+    public WebElement inputPhoneNumber;
+    @FindBy(xpath = "//textarea[@id='comment']")
+    public WebElement inputCommit;
+    @FindBy(xpath = "//button[@title='Submit']")
+    public WebElement SubmitButton;
+    @FindBy(xpath = "//div[@class='page messages']")
+    public WebElement Message;public void doContactUs(String name, String email, String phoneNumber, String commit){
+        safeClickOnElement(contactUsButton);
+        sendKeysToElement(inputName,name);
+        sendKeysToElement(inputEmail,email);
+        sendKeysToElement(inputPhoneNumber, phoneNumber);
+        sendKeysToElement(inputCommit, commit);
+        safeClickOnElement(SubmitButton);
+        safeClickOnElement(Message);
+
+    }
+
+    }
+
